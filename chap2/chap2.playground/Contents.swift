12130366f5950511369cca868071cf0759b08f14
Nikolai Schlegel <nikolai.schlegel@gmail.com>
2023-11-20 13:52:50 -0800

    Chapter 2

---
+++ chap2/chap2.playground/Contents.swift
@@ -0,0 +1,10 @@
+import Cocoa
+
+let numberOfStoplights: Int = 4
+var population: Int
+population = 5422
+var elevation: Int = 154
+let townName: String = "Knowwhere"
+let townDescription =
+    "\(townName) has a population of \(population) and \(numberOfStoplights) stoplights. It is located at \(elevation) ft"
+print(townDescription)
