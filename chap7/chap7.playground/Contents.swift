105d81e6279e8cd097e80de93d47c6217686f58d
Nikolai Schlegel <nikolai.schlegel@gmail.com>
2023-11-22 12:42:59 -0800

    Chap 7 & 8

--- chap7/chap7.playground/Contents.swift
+++ chap7/chap7.playground/Contents.swift
@@ -22,3 +22,28 @@ let b = (aAcute == aAcutePrecomposed)
 aAcute.count
 aAcutePrecomposed.count
 
+let start = playground.startIndex
+let end = playground.index(start, offsetBy: 4)
+let fifthCharacter = playground[end]
+let range = ...end
+let firstFive = playground[range]
+
+let empty = ""
+let startEmpty = empty.startIndex
+let endEmpty = empty.endIndex
+if (startEmpty == endEmpty) {
+    print("string is empty")
+}
+
+let helloString = "\u{0048}\u{0065}\u{006C}\u{006C}\u{006F}"
+
+let startPlay = playground.index(playground.startIndex, offsetBy: 7)
+let endPlay = playground.index(startPlay, offsetBy: 3)
+let playRange = startPlay...endPlay
+let play = playground[playRange]
+
+let tale = """
+    It was the best of times,
+    It was the worst of times,
+    """
+print(tale)
---
+++ chap8/chap8.playground/Contents.swift
@@ -0,0 +1,56 @@
+import Cocoa
+
+var bucketList = ["Climb Mt. Everest"]
+bucketList.append("Read War and Peace")
+bucketList.append("Go on and Artic expedition")
+bucketList.append("Scuba dive in the Great Blue Hole")
+bucketList.append("Find a triple rainbow")
+bucketList.count
+bucketList.remove(at: 1)
+bucketList
+print(bucketList[...2])
+bucketList[1] += " with friends"
+bucketList[1]
+bucketList[0] = "Climb Mt. Kilimanjaro"
+bucketList.insert("Toboggan across Alaska", at: 1)
+bucketList
+
+var newItems = [
+    "Bike across America",
+    "Make a perfect souffle",
+    "Solve Fermat's enigma"
+]
+
+bucketList += newItems
+print(bucketList)
+
+var anotherList = [
+    "Bike across America",
+    "Make a perfect souffle",
+    "Solve Fermat's enigma"
+]
+
+newItems == anotherList
+
+let lunches = [
+    "Cheeseburger",
+    "Veggie Pizza",
+    "Chicken Caesar Salad",
+    "Black Bean Burrito",
+    "Falafel Wrap"
+]
+
+var todoList = ["Take out the trash", "Pay bills", "Cross off finished items"]
+todoList.isEmpty
+var t = todoList.count
+repeat {
+    t -= 1
+    print(todoList[t])
+
+} while (t > 0)
+todoList.reverse()
+print(todoList)
+todoList.shuffle()
+print(todoList)
+
+
